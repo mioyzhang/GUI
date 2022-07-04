2c61e359489dc6d003b1c99da78995319b0b89ab
mioyzhang <32668636+mioyzhang@users.noreply.github.com>
2022-07-04 17:28:15 +0800

    Update t1.py

--- t1.py
+++ t1.py
@@ -1,55 +1,62 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-# @Project : pypj
-# @File    : multi_service_test.py
-# @IDE     : PyCharm
-# @Author  : zhang bin
-# @Date    : 2022/7/4 15:24:53
-# @DES     : 
-"""
 import time
 import threading
 import paramiko
 
 
-def service_test():
-    print('b')
-    for i in range(5):
-        print(i)
-        time.sleep(0.2)
-    print('e')
-    pass
+class TestThread(threading.Thread):
+    def __init__(self, ip, port='22', username='root', password='root'):
+        super(TestThread, self).__init__()
+        self.ip = ip
+        self.port = port
+        self.username = username
+        self.password = password
+
+        try:
+            self.transport = paramiko.Transport((self.ip, 22))
+            self.transport.connect(username='root', password='root')
+            self.sftp = paramiko.SFTPClient.from_transport(self.transport)
+        except BaseException as e:
+            print(e)
+            raise Exception('连接失败')
+
+    def file_download(self, remotepath='/home/dell/2019-05-28-15-36-07.dat', localpath='1.txt'):
+        print('Download file: {} --> {}'.format(remotepath, localpath))
+        self.sftp.get(remotepath, localpath, callback=view)
+
+    def close(self):
+        self.sftp.close()
+        self.transport.close()
+
+    def run(self):
+        self.service_test()
+
+    def service_test(self):
+        print('b')
+        for i in range(self.ip):
+            print(i)
+            time.sleep(0.2)
+        print('e')
+        pass
 
 
-def test():
+def view(transferred, full_size):
+
+    print('Transferred:{}/{}\r'.format(transferred, full_size))
+
+
+def main():
     print('test begin')
-    p = [threading.Thread(target=service_test) for _ in range(3)]
-    for i in p:
+    t = [TestThread(5) for _ in range(3)]
+    for i in t:
         i.start()
-    for i in p:
+    for i in t:
         i.join()
     print('test end')
 
 
-def view(transferred, toBeTransferred):
-    print('Transferred:{}/{}\r'.format(transferred, toBeTransferred))
-
-
-def file_download(ip):
-    transport = paramiko.Transport((ip, 22))
-    transport.connect(username='root', password='root')
-
-    sftp = paramiko.SFTPClient.from_transport(transport)
-    remotepath = '/home/dell/2019-05-28-15-36-07.dat'
-    localpath = '1.txt'
-    sftp.get(remotepath, localpath, callback=view)
-
-    sftp.close()
-    transport.close()
-
-
 if __name__ == '__main__':
-    # test()
-    file_download('172.27.1.1')
+    # main()
+    t = TestThread('172.27.1.1')
+    t.file_download()
+    t.close()
     pass
